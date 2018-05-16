468df1a3c57354b2b5278e003811e85cb7715c16
Dmitry Kunin <dkunin@avito.ru>
2018-05-17 01:09:32 +0300

    Update

--- src/main.js
+++ src/main.js
@@ -4,3 +4,19 @@ import app from './app.js';
 // Vue.component('monsterShortStat', monsterShortStat);
 Vue.use(VueResource);
 new Vue(app);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('../sw.js')
+        .then(function(registration) {
+            // Registration Success
+            console.log(
+                '[serviceWorker]: registration successful with scope: ',
+                registration.scope
+            );
+        })
+        .catch(function(err) {
+            // Registration failed :(
+            console.log('[serviceWorker] registration failed', err);
+        });
+}
