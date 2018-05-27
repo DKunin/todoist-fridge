60673b923be5bb00722923848f499aeede1f7ac0
Dmitry Kunin <dkunin@avito.ru>
2018-05-27 22:39:34 +0300

    Update the service worker out of the module

--- src/main.js
+++ src/main.js
@@ -4,19 +4,3 @@ import app from './app.js';
 // Vue.component('monsterShortStat', monsterShortStat);
 Vue.use(VueResource);
 new Vue(app);
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('../sw.js')
-        .then(function(registration) {
-            // Registration Success
-            console.log(
-                '[serviceWorker]: registration successful with scope: ',
-                registration.scope
-            );
-        })
-        .catch(function(err) {
-            // Registration failed :(
-            console.log('[serviceWorker] registration failed', err);
-        });
-}
